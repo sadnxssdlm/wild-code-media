60f63c3e480288ccca0aa32140d5c680019f15b0
nicoletta <davidsalnicole@gmail.com>
2025-03-26 18:46:58 +0100

    correction and Update of CSS file with double insertion after merging into main

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const menuHamburger = document.querySelector(".menu-hamburger");
 const navLinks = document.querySelector(".nav-links");
 
 menuHamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("mobile-menu");
+  navLinks.classList.toggle("mobile-menu");
 });
 /*HEADER */
 
@@ -13,54 +13,56 @@ menuHamburger.addEventListener("click", () => {
 /* change d'image quand on slide  */
 /*Quand tu passe ta souris sur l'image elle s'agrandi */
 
-const slide = ["slider1.jpg", "slider2.avif", "slider3.jpg", "slider4.avif", "slider5.jpg", "slider6.avif"];
-let numero = 0;
+const slide = [
+  "slider1.jpg",
+  "slider2.avif",
+  "slider3.jpg",
+  "slider4.avif",
+  "slider5.jpg",
+  "slider6.avif",
+];
+let index = 0;
 function changeSlide(sens) {
-    numero = numero + sens;
-    if (numero > slide.length - 1)
-        numero = 0;
-    if (numero < 0)
-        numero = slide.length - 1;
-    document.getElementById("slider1").src = "assets/img/images/" + slide[numero];
+  index = index + sens;
+  if (index > slide.length - 1) index = 0;
+  if (index < 0) index = slide.length - 1;
+  document.getElementById("slider1").src = "assets/img/images/" + slide[numero];
 }
 /* changes d'images toutes les 4sec (4000)  */
 setInterval("changeSlide(1)", 4000);
 /*STORIES */
 
-
 // Toutes les variables
-const stories = document.querySelectorAll('.story');
-const storiesModal = document.querySelector('.stories-modal');
-const modalStory = document.getElementById('modalstory');
-const closeBtn = document.querySelector('.close-btn');
-
+const stories = document.querySelectorAll(".story");
+const storiesModal = document.querySelector(".stories-modal");
+const modalStory = document.getElementById("modalstory");
+const closeBtn = document.querySelector(".close-btn");
 
 // Ouvrir le modal
-stories.forEach(story => {
-  story.addEventListener('click', () => {
+stories.forEach((story) => {
+  story.addEventListener("click", () => {
     modalStory.src = story.src;
-    storiesModal.style.display = 'flex';
-    closeBtn.style.display = 'block';
+    storiesModal.style.display = "flex";
+    closeBtn.style.display = "block";
 
     // Vu (retire les contours)
-    story.classList.add('viewed');
+    story.classList.add("viewed");
   });
 });
 
 // Fermer le modal
-closeBtn.addEventListener('click', () => {
-  storiesModal.style.display = 'none';
-  closeBtn.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+  storiesModal.style.display = "none";
+  closeBtn.style.display = "none";
 });
 
 // Quand tu cliques en dehors du model il se ferme
-storiesModal.addEventListener('click', (e) => {
+storiesModal.addEventListener("click", (e) => {
   if (e.target === storiesModal) {
-    storiesModal.style.display = 'none';
-    closeBtn.style.display = 'none';
+    storiesModal.style.display = "none";
+    closeBtn.style.display = "none";
   }
 });
-
 
 /*POSTS */
 
@@ -85,51 +87,19 @@ function countClicks() {
   console.log((like.innerHTML = count));
 }
 
-// document.querySelectorAll(".reactpost1").forEach((post) => {
-//   const ratings = post.querySelectorAll(".post-rating-count");
-//   const likeRating = ratings[0];
-//   const button = post.querySelector(".btnh1");
-//   const count = post.querySelector(".post-rating-count");
-
-//   ratings.forEach((rating) => {
-//     if (rating.classList.contains("Rating")) {
-//       const count = rating.querySelector(".post-rating-count");
-
-//       count.textContent = Number(count.textContent);
-//       rating.classList.remove("Rating");
-//     }
-//   });
-// });
-
 if (heart1.style.color === "red") {
-        heart1.style.color = "grey";
-    } else {
-        heart1.style.color = "red";
-    }
+  heart1.style.color = "grey";
+} else {
+  heart1.style.color = "red";
 }
+
 function postRating() {
-    if (heart1 === "red") {
-        heart1 + 1;
-    } else {
-        heart1 - 1;
-    }
+  if (heart1 === "red") {
+    heart1 + 1;
+  } else {
+    heart1 - 1;
+  }
 }
-
-document.querySelectorAll(".reactpost1").forEach((post) => {
-    const ratings = post.querySelectorAll(".post-rating-count");
-    const likeRating = ratings[0];
-    const button = post.querySelector(".btnh1");
-    const count = post.querySelector(".post-rating-count");
-
-    ratings.forEach((rating) => {
-        if (rating.classList.contains("Rating")) {
-            const count = rating.querySelector(".post-rating-count");
-
-            count.textContent = Number(count.textContent);
-            rating.classList.remove("Rating");
-        }
-    });
-});
 
 /*post 2*/
 
@@ -138,11 +108,11 @@ document.querySelectorAll(".reactpost1").forEach((post) => {
 /*like*/
 let heart2 = document.getElementById("btnh2");
 function Toggle2() {
-    if (heart2.style.color === "red") {
-        heart2.style.color = "grey";
-    } else {
-        heart2.style.color = "red";
-    }
+  if (heart2.style.color === "red") {
+    heart2.style.color = "grey";
+  } else {
+    heart2.style.color = "red";
+  }
 }
 
 /*post 3*/
@@ -152,45 +122,45 @@ function Toggle2() {
 /*like*/
 let heart3 = document.getElementById("btnh3");
 function Toggle3() {
-    if (heart3.style.color === "red") {
-        heart3.style.color = "grey";
-    } else {
-        heart3.style.color = "red";
-    }
+  if (heart3.style.color === "red") {
+    heart3.style.color = "grey";
+  } else {
+    heart3.style.color = "red";
+  }
 }
 
 /*Retweet*/
 
 /*WILDERSMSG */
-let comment = ["La formation en alternance m'a permis d'avoir une première expérience professionnelle dans le développement web, de continuer à approfondir mes connaissances et d'apprendre des nouvelles notions pendant une année supplémentaire.",
-    "Même si on était en distanciel, on échangeait énormément, que ce soit avec nos camarades ou le formateur qui était vraiment pédagogue et présent pour nous à n'importe quel moment.",
-    "Allez chercher de la confiance auprès des autres apprenants avec vous, pour éviter ce syndrome de l'imposteur par lequel on est tous passés à un moment donné. Et surtout, accrochez-vous ! C'est très intense, mais on apprend plein de choses et on ressort de là avec énormément de connaissances, c'est vraiment génial.",
-    "J’ai vécu une expérience intense riche en émotion pendant 5 mois de laquelle je suis devenu quelqu’un de meilleur autant sur le plan personnel que professionnel. C’est pourquoi j’ai décidé de remettre le couvert pendant un an en accordant de nouveau ma confiance à la Wild Code School pour la poursuite de mes études."
+let comment = [
+  "La formation en alternance m'a permis d'avoir une première expérience professionnelle dans le développement web, de continuer à approfondir mes connaissances et d'apprendre des nouvelles notions pendant une année supplémentaire.",
+  "Même si on était en distanciel, on échangeait énormément, que ce soit avec nos camarades ou le formateur qui était vraiment pédagogue et présent pour nous à n'importe quel moment.",
+  "Allez chercher de la confiance auprès des autres apprenants avec vous, pour éviter ce syndrome de l'imposteur par lequel on est tous passés à un moment donné. Et surtout, accrochez-vous ! C'est très intense, mais on apprend plein de choses et on ressort de là avec énormément de connaissances, c'est vraiment génial.",
+  "J’ai vécu une expérience intense riche en émotion pendant 5 mois de laquelle je suis devenu quelqu’un de meilleur autant sur le plan personnel que professionnel. C’est pourquoi j’ai décidé de remettre le couvert pendant un an en accordant de nouveau ma confiance à la Wild Code School pour la poursuite de mes études.",
 ];
 
 let texte = document.querySelector(".txt1");
-let numero = 0; 
-let indexLettre = 0; 
+let numero = 0;
+let indexLettre = 0;
 
 function ecrirePhrase() {
-    let contenu = comment[numero]; 
-    texte.innerHTML = ""; 
-    indexLettre = 0;
+  let contenu = comment[numero];
+  texte.innerHTML = "";
+  indexLettre = 0;
 
-    let timer = setInterval(() => {
-        if (indexLettre < contenu.length) {
-            texte.innerHTML += contenu.charAt(indexLettre);
-            indexLettre++;
-        } else {
-            clearInterval(timer);
-            setTimeout(() => {
-                numero = (numero + 1) % comment.length; 
-                ecrirePhrase(); 
-            }, 3000); 
-        }
-    }, 50);
+  let timer = setInterval(() => {
+    if (indexLettre < contenu.length) {
+      texte.innerHTML += contenu.charAt(indexLettre);
+      indexLettre++;
+    } else {
+      clearInterval(timer);
+      setTimeout(() => {
+        numero = (numero + 1) % comment.length;
+        ecrirePhrase();
+      }, 3000);
+    }
+  }, 50);
 }
-
 ecrirePhrase();
 
 /*FOOTER */
